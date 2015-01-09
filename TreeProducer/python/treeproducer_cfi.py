1b0432625d9de35416b359cac61839d0452104f9
ndaci <daci.nadir@gmail.com>
2015-01-09 10:36:55 +0100

    Remove useless labels.

--- TreeProducer/python/treeproducer_cfi.py
+++ TreeProducer/python/treeproducer_cfi.py
@@ -3,8 +3,5 @@ import FWCore.ParameterSet.Config as cms
 treep = cms.EDAnalyzer(
     'TreeProducer',
     hltProcessName   = cms.string("HLT"),
-    genParticleLabel = cms.string("genParticles"),
-    pfjetCollection    = cms.InputTag("ak5PFJets"),
-    pfmetCollection    = cms.InputTag("pfMet"),
-    muCollection     = cms.InputTag("muons")
+    pfjetCollection    = cms.InputTag("ak5PFJets")
 )
