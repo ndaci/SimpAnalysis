654a4bb7e249a8ab9e1449159f5a81d06de92ab4
ndaci <daci.nadir@gmail.com>
2015-01-09 10:37:21 +0100

    Correct parameter names.

--- SkimJets/test/crab_JetHT_2012D.py
+++ SkimJets/test/crab_JetHT_2012D.py
@@ -7,7 +7,7 @@ name = 'Simps_JetHT_2012D_08Jan2015_v1'
 config.section_("General")
 config.General.requestName = name 
 config.General.workArea    = '/user/ndaci/CRABBY/Simps/Data/JetHT/'
-config.General.saveLogs    = True
+config.General.transferLogs = True
 #config.General.transferOutput = True
 
 # JOB TYPE
@@ -21,11 +21,11 @@ config.JobType.allowNonProductionCMSSW = True
 # INPUT DATA
 config.section_("Data")
 config.Data.inputDataset = '/JetHT/Run2012D-22Jan2013-v1/AOD'
-config.Data.dbsUrl = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
+config.Data.inputDBS  = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 500
 config.Data.publication = True
-config.Data.publishDbsUrl = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
+config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
 config.Data.publishDataName = name
 config.Data.ignoreLocality = False # allows to process inputs on CE != site hosting inputs
 config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions12/8TeV/Reprocessing/Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt'
@@ -36,7 +36,7 @@ config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/
 
 # USER
 config.section_("User")
-config.User.email = 'nadir.daci@cern.ch'
+#config.User.email = 'nadir.daci@cern.ch'
 #config.User.voRole = 
 config.User.voGroup = 'becms'
 
