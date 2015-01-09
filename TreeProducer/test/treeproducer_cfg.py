2c5366a13352ef22dc2f1933b347d3885aba7e98
ndaci <daci.nadir@gmail.com>
2015-01-09 12:01:34 +0100

    Put path to an actual file. Add output tfileservice.

--- TreeProducer/test/treeproducer_cfg.py
+++ TreeProducer/test/treeproducer_cfg.py
@@ -1,16 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("Demo")
+process = cms.Process("SIMPTREE")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(5) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        'file:myfile.root'
+        '/store/user/ndaci/JetHT/Simps_JetHT_2012D_08Jan2015_v1/150108_171434/0000/skimjets_122.root'
     )
 )
 
+# Tree producer
 process.load("SimpAnalysis.TreeProducer.treeproducer_cfi") 
 process.p = cms.Path(process.treep)
+
+# Output
+process.TFileService = cms.Service('TFileService',
+    fileName = cms.string('file:simptree.root')
+)
