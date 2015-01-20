f5f387e860ec851c71a81279d179b1dbd9105c36
ndaci <daci.nadir@gmail.com>
2015-01-20 10:36:23 +0100

    Change name of EDAnalyzer instance.

--- TreeProducer/python/treeproducer_cfi.py
+++ TreeProducer/python/treeproducer_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-treep = cms.EDAnalyzer(
+tree = cms.EDAnalyzer(
     'TreeProducer',
     hltProcessName   = cms.string("HLT"),
     pfjetCollection  = cms.InputTag("ak5PFJets"),
