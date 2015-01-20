b31474d549f16cc0edc33a1aae7317de1b25dce3
ndaci <daci.nadir@gmail.com>
2015-01-20 10:37:41 +0100

    Change input file, max events, output file name, EDAnalyzer instance name.

--- TreeProducer/test/treeproducer_cfg.py
+++ TreeProducer/test/treeproducer_cfg.py
@@ -4,19 +4,21 @@ process = cms.Process("SIMPTREE")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(5) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/user/ndaci/JetHT/Simps_JetHT_2012D_08Jan2015_v1/150108_171434/0000/skimjets_122.root'
+        '/store/user/ndaci/JetHT/Simps_JetHT_2012D_v4_13Jan2015/150113_110917/0000/skimjets_52.root'
+        #'/store/user/ndaci/JetHT/Simps_JetHT_2012D_08Jan2015_v1/150108_171434/0000/skimjets_122.root'
+        #'file:skimjets.root'
     )
 )
 
 # Tree producer
 process.load("SimpAnalysis.TreeProducer.treeproducer_cfi") 
-process.p = cms.Path(process.treep)
+process.p = cms.Path(process.tree)
 
 # Output
 process.TFileService = cms.Service('TFileService',
-    fileName = cms.string('file:simptree.root')
+    fileName = cms.string('simptree.root')
 )
