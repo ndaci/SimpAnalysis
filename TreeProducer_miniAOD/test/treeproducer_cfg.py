58e0bf01a0638a441583fc6378ab57e2125e13bc
Isabelle <isa.dbruyn@gmail.com>
2016-09-27 15:35:37 +0200

    change name to TreeProducer_miniAOD

--- TreeProducer_miniAOD/test/treeproducer_cfg.py
+++ TreeProducer_miniAOD/test/treeproducer_cfg.py
@@ -4,18 +4,19 @@ process = cms.Process("SIMPTREE")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-        '/store/user/ndaci/JetHT/Simps_JetHT_2012D_v4_13Jan2015/150113_110917/0000/skimjets_52.root'
-        #'/store/user/ndaci/JetHT/Simps_JetHT_2012D_08Jan2015_v1/150108_171434/0000/skimjets_122.root'
-        #'file:skimjets.root'
-    )
+    #fileNames = cms.untracked.vstring(
+        #'/store/user/ndaci/JetHT/Simps_JetHT_2012D_v4_13Jan2015/150113_110917/0000/skimjets_52.root'
+        ##'/store/user/ndaci/JetHT/Simps_JetHT_2012D_08Jan2015_v1/150108_171434/0000/skimjets_122.root'
+        ##'file:skimjets.root'
+    #)
+    fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/data/Run2016D/JetHT/MINIAOD/PromptReco-v2/000/276/315/00000/208AB2DD-0145-E611-910F-02163E0144AD.root')
 )
 
 # Tree producer
-process.load("SimpAnalysis.TreeProducer.treeproducer_cfi") 
+process.load("SimpAnalysis.TreeProducer_miniAOD.Treeproducer_miniAOD_cfi") 
 process.p = cms.Path(process.tree)
 
 # Output
