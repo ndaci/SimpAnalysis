d5bd93e68835f345eb085c6e69d0ecc37ef8b20f
Isabelle <isa.dbruyn@gmail.com>
2016-10-19 21:53:16 +0200

    filter events with our trigger paths

--- TreeProducer_miniAOD/test/treeproducer_cfg.py
+++ TreeProducer_miniAOD/test/treeproducer_cfg.py
@@ -4,20 +4,44 @@ process = cms.Process("SIMPTREE")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
 process.source = cms.Source("PoolSource",
-    #fileNames = cms.untracked.vstring(
-        #'/store/user/ndaci/JetHT/Simps_JetHT_2012D_v4_13Jan2015/150113_110917/0000/skimjets_52.root'
-        ##'/store/user/ndaci/JetHT/Simps_JetHT_2012D_08Jan2015_v1/150108_171434/0000/skimjets_122.root'
-        ##'file:skimjets.root'
-    #)
-    fileNames = cms.untracked.vstring('/store/data/Run2016D/JetHT/MINIAOD/PromptReco-v2/000/276/315/00000/283C90B1-0445-E611-B0AF-02163E014393.root')
+    fileNames = cms.untracked.vstring(
+                 '/store/data/Run2016D/JetHT/MINIAOD/PromptReco-v2/000/276/315/00000/283C90B1-0445-E611-B0AF-02163E014393.root',
+                 '/store/data/Run2016D/JetHT/MINIAOD/PromptReco-v2/000/276/315/00000/48DA42E9-0145-E611-B53F-02163E011A4F.root')
 )
 
+#process.triggerSelection = cms.EDFilter( "TriggerResultsFilter",
+    #triggerConditions = cms.vstring(
+      #'HLT_DiCentralPFJet170_CFMax0p1_v*',
+      #'HLT_DiCentralPFJet220_CFMax0p3_v*',
+      #'HLT_DiCentralPFJet330_CFMax0p5_v*',
+      #'HLT_DiCentralPFJet430_v*',
+      #'HLT_DiCentralPFJet170_v*',
+      #'HLT_SingleCentralPFJet170_CFMax0p1_v*'),
+    #hltResults = cms.InputTag( "TriggerResults", "", "HLT" ),
+    #l1tResults = cms.InputTag( "" ),
+    #l1tIgnoreMask = cms.bool( False ),
+    #l1techIgnorePrescales = cms.bool( False ),
+    #daqPartitions = cms.uint32( 1 ),
+    #throw = cms.bool( True )
+#)
+
+import HLTrigger.HLTfilters.hltHighLevel_cfi
+process.triggerSelection =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+    HLTPaths = ['HLT_DiCentralPFJet170_CFMax0p1_v*',
+      'HLT_DiCentralPFJet220_CFMax0p3_v*',
+      'HLT_DiCentralPFJet330_CFMax0p5_v*',
+      'HLT_DiCentralPFJet430_v*',
+      'HLT_DiCentralPFJet170_v*',
+      'HLT_SingleCentralPFJet170_CFMax0p1_v*'],
+    throw = False
+)
+    
 # Tree producer
 process.load("SimpAnalysis.TreeProducer_miniAOD.Treeproducer_miniAOD_cfi") 
-process.p = cms.Path(process.tree)
+process.p = cms.Path(process.triggerSelection * process.tree)
 
 # Output
 process.TFileService = cms.Service('TFileService',
