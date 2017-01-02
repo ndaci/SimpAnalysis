882bed2bde10aa42c2cc07b42db8714e7fc619d7
Isabelle <isa.dbruyn@gmail.com>
2017-01-02 12:32:14 +0100

    cfg for AOD analyzer

---
+++ TreeProducer_AOD/test/treeproducer_AOD_MC_cfg.py
@@ -0,0 +1,49 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("SIMPTREE")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000000) )
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+										'/store/mc/RunIISummer16DR80Premix/QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/AODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/110000/00086432-1CB2-E611-9E62-485B39897219.root')
+)
+
+#process.triggerSelection = cms.EDFilter( "TriggerResultsFilter",
+    #triggerConditions = cms.vstring(
+      #'HLT_DiCentralPFJet170_CFMax0p1_v*',
+      #'HLT_DiCentralPFJet220_CFMax0p3_v*',
+      #'HLT_DiCentralPFJet330_CFMax0p5_v*',
+      #'HLT_DiCentralPFJet430_v*',
+      #'HLT_DiCentralPFJet170_v*',
+      #'HLT_SingleCentralPFJet170_CFMax0p1_v*'),
+    #hltResults = cms.InputTag( "TriggerResults", "", "HLT" ),
+    #l1tResults = cms.InputTag( "" ),
+    #l1tIgnoreMask = cms.bool( False ),
+    #l1techIgnorePrescales = cms.bool( False ),
+    #daqPartitions = cms.uint32( 1 ),
+    #throw = cms.bool( True )
+#)
+
+#import HLTrigger.HLTfilters.hltHighLevel_cfi
+#process.triggerSelection =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+    #HLTPaths = ['HLT_DiCentralPFJet170_CFMax0p1_v*',
+      #'HLT_DiCentralPFJet220_CFMax0p3_v*',
+      #'HLT_DiCentralPFJet330_CFMax0p5_v*',
+      #'HLT_DiCentralPFJet430_v*',
+      #'HLT_DiCentralPFJet170_v*',
+      #'HLT_SingleCentralPFJet170_CFMax0p1_v*'],
+    #throw = False
+#)
+    
+# Tree producer
+process.load("SimpAnalysis.TreeProducer_AOD.Treeproducer_AOD_cfi") 
+#process.tree.triggerResults = cms.InputTag("TriggerResults", "", "HLT2") #for XXTo4J
+process.p = cms.Path(process.tree)
+
+# Output
+process.TFileService = cms.Service('TFileService',
+    fileName = cms.string('QCD_PUMoriond17_AOD.root')
+)
