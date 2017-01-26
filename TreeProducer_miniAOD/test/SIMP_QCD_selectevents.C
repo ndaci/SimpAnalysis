8db7cf494c33e4abbf0cca0bc7a765056b7573b9
Isabelle <isa.dbruyn@gmail.com>
2017-01-26 10:26:27 +0100

    add photon veto

--- TreeProducer_miniAOD/test/SIMP_QCD_selectevents.C
+++ TreeProducer_miniAOD/test/SIMP_QCD_selectevents.C
@@ -17,12 +17,19 @@
 // #include "list_QCD_1500To2000.h"
 // #include "list_QCD_2000ToInf.h"
 
-#include "list_QCD_300To500_PUMoriond17.h"
-#include "list_QCD_500To700_PUMoriond17.h"
-#include "list_QCD_700To1000_PUMoriond17.h"
-#include "list_QCD_1000To1500_PUMoriond17.h"
-#include "list_QCD_1500To2000_PUMoriond17.h"
-#include "list_QCD_2000ToInf_PUMoriond17.h"
+// #include "list_QCD_300To500_PUMoriond17.h"
+// #include "list_QCD_500To700_PUMoriond17.h"
+// #include "list_QCD_700To1000_PUMoriond17.h"
+// #include "list_QCD_1000To1500_PUMoriond17.h"
+// #include "list_QCD_1500To2000_PUMoriond17.h"
+// #include "list_QCD_2000ToInf_PUMoriond17.h"
+
+#include "lists/list_QCD_300To500_PUMoriond17_photoninfo.h"
+#include "lists/list_QCD_500To700_PUMoriond17_photoninfo.h"
+#include "lists/list_QCD_700To1000_PUMoriond17_photoninfo.h"
+#include "lists/list_QCD_1000To1500_PUMoriond17_photoninfo.h"
+#include "lists/list_QCD_1500To2000_PUMoriond17_photoninfo.h"
+#include "lists/list_QCD_2000ToInf_PUMoriond17_photoninfo.h"
 
 void SIMP_QCD_selectevents(){
   
@@ -46,6 +53,12 @@ void SIMP_QCD_selectevents(){
   double jet_pt[8], jet_eta[8], jet_phi[8], jet_efrac_ch_Had[8], jet_efrac_ch_EM[8], jet_efrac_ch_Mu[8];
 	double CHEF_jet[8];
 	double track_pt[10], track_ptError[10];
+	int nPixHits[10];
+	double photon_eta[4], photon_phi[4], photon_pt[4];
+	int passLooseId[4], passMediumId[4], passTightId[4];
+	
+	int picked[6] = {0, 0, 0, 0, 0, 0};
+	int selected[6] = {0, 0, 0, 0, 0, 0};
 	
 // 	std::cout<<std::endl;
 	
@@ -57,7 +70,7 @@ void SIMP_QCD_selectevents(){
 // // 	double QCD_events[5] = {16830696, 19199088, 15621634, 4980387, 3846616};
 // 	double lumi = 20;
   
-	for (int l = 2; l < 3; l++){
+	for (int l = 0; l < 6; l++){
 		std::cout<<"QCD bin "<<l<<"..."<<std::flush;
 		TChain* chain = chains[l];
 		chain->SetBranchAddress("nRun", &run);
@@ -71,6 +84,13 @@ void SIMP_QCD_selectevents(){
 		chain->SetBranchAddress("jet_efrac_ch_Mu", &jet_efrac_ch_Mu);
 		chain->SetBranchAddress("track_pt", &track_pt);
 		chain->SetBranchAddress("track_ptError", &track_ptError);
+		chain->SetBranchAddress("track_nPixHits", &nPixHits);
+    chain->SetBranchAddress("photon_pt", &photon_pt);
+    chain->SetBranchAddress("photon_eta", &photon_eta);
+    chain->SetBranchAddress("photon_phi", &photon_phi);
+		chain->SetBranchAddress("photon_passLooseId",&passLooseId);
+		chain->SetBranchAddress("photon_passMediumId",&passMediumId);
+		chain->SetBranchAddress("photon_passTightId",&passTightId);
 		
 		Int_t Nentries = chain->GetEntries(); 
 		std::cout<<"processing "<<Nentries<<std::endl;
@@ -81,18 +101,37 @@ void SIMP_QCD_selectevents(){
 			double deltajet_phi = jet_phi[0] - jet_phi[1];
 			if(deltajet_phi > TMath::Pi()) deltajet_phi -= 2*TMath::Pi();
 			if(deltajet_phi < -TMath::Pi()) deltajet_phi += 2*TMath::Pi();
+			deltajet_phi = fabs(deltajet_phi);
+			
+			double deltaphi_jet1photon = jet_phi[0] - photon_phi[0];
+			if(deltaphi_jet1photon > TMath::Pi()) deltaphi_jet1photon -= 2*TMath::Pi();
+			if(deltaphi_jet1photon < -TMath::Pi()) deltaphi_jet1photon += 2*TMath::Pi();
+			double deltaphi_jet2photon = jet_phi[1] - photon_phi[0];
+			if(deltaphi_jet2photon > TMath::Pi()) deltaphi_jet2photon -= 2*TMath::Pi();
+			if(deltaphi_jet2photon < -TMath::Pi()) deltaphi_jet2photon += 2*TMath::Pi();
+			
+			double deltaeta_jet1photon = jet_eta[0] - photon_eta[0];
+			double deltaeta_jet2photon = jet_eta[1] - photon_eta[0];
+			
+			double dR1 = TMath::Sqrt(deltaphi_jet1photon*deltaphi_jet1photon + deltaeta_jet1photon*deltaeta_jet1photon);
+			double dR2 = TMath::Sqrt(deltaphi_jet2photon*deltaphi_jet2photon + deltaeta_jet2photon*deltaeta_jet2photon);
 			
 			for (int i = 0; i < 8; i++){
 				CHEF_jet[i] = jet_efrac_ch_Had[i]+jet_efrac_ch_EM[i]+jet_efrac_ch_Mu[i];
 			} 
 			
-			if (jet_pt[0] > 250 && jet_pt[1] > 250 && fabs(jet_eta[0]) < 2.0 && fabs(jet_eta[1]) < 2.0 && deltajet_phi > 2){
+			if (jet_pt[0] > 250 && jet_pt[1] > 250 && fabs(jet_eta[0]) < 2.0 && fabs(jet_eta[1]) < 2.0 && deltajet_phi > 2 && nPixHits[0] > 0 && (passLooseId[0] == 0 || (passLooseId[0] == 1 && dR1 > 0.1 && dR2 > 0.1))){
+				selected[l]++;
 // 				if(track_ptError[0]/track_pt[0] > 0.5) std::cout<<run<<":"<<LS<<":"<<event<<std::endl;
-				if((CHEF_jet[0]<0.2 && CHEF_jet[1]<0.2)&&(CHEF_jet[1]<0.01 || CHEF_jet[0]<0.01)) std::cout<<run<<":"<<LS<<":"<<event<<std::endl;
+				if((CHEF_jet[0]<0.2 && CHEF_jet[1]<0.2)&&(CHEF_jet[1]<0.01 || CHEF_jet[0]<0.01)){
+					picked[l]++;
+					std::cout<<run<<":"<<LS<<":"<<event<<std::endl;
+				}
 // 				if((CHEF_jet[0]>0.5 && CHEF_jet[1]<0.01)||(CHEF_jet[1]>0.5 && CHEF_jet[0]<0.01)) std::cout<<run<<":"<<LS<<":"<<event<<",";
 // 				if(CHEF_jet[0]>0.5 && CHEF_jet[1]>0.5) std::cout<<run<<":"<<LS<<":"<<event<<", "<<std::flush;
 			}    
 		}
+		std::cout<<"picked: "<<(double)picked[l]/(double)selected[l]<<std::endl;
 		std::cout<<std::endl;
 	}
 }
