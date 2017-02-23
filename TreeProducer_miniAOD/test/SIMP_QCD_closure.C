5d7bc5564bcbd7de3294a5bb367f8d42b137b7b2
Isabelle <isa.dbruyn@gmail.com>
2017-02-23 11:18:18 +0100

    added photon veto and ratio plot

--- TreeProducer_miniAOD/test/SIMP_QCD_closure.C
+++ TreeProducer_miniAOD/test/SIMP_QCD_closure.C
@@ -25,6 +25,13 @@
 #include "lists/list_QCD_1500To2000_PUMoriond17.h"
 #include "lists/list_QCD_2000ToInf_PUMoriond17.h"
 
+// #include "lists/list_QCD_300To500_PUMoriond17_photoninfo.h"
+// #include "lists/list_QCD_500To700_PUMoriond17_photoninfo.h"
+// #include "lists/list_QCD_700To1000_PUMoriond17_photoninfo.h"
+// #include "lists/list_QCD_1000To1500_PUMoriond17_photoninfo.h"
+// #include "lists/list_QCD_1500To2000_PUMoriond17_photoninfo.h"
+// #include "lists/list_QCD_2000ToInf_PUMoriond17_photoninfo.h"
+
 void SIMP_QCD_closure(){
   
   TChain* chain0 = new TChain("tree/SimpAnalysis");
@@ -42,8 +49,14 @@ void SIMP_QCD_closure(){
 	TChain* chains[6] = {chain0, chain1, chain2, chain3, chain4, chain5};
 	std::cout<<"TChains ready"<<std::endl;
   
+  bool badEvent = false;
+  int LS, event;
+	int dijet_170;
   double jet_pt[8], jet_eta[8], jet_phi[8], jet_efrac_ch_Had[8], jet_efrac_ch_EM[8], jet_efrac_ch_Mu[8], CHEF_jet[8];
-	double track_pt[10], track_ptError[10];
+	double track_pt[10], track_ptError[10], track_dz[10], track_d0[10], track_dxy[10];
+	int npixhits[10];
+	double photon_eta[4], photon_phi[4], photon_pt[4];
+	int passLooseId[4], passMediumId[4], passTightId[4];
 	
 	double chf_cuts[11] = {0.5, 0.4, 0.3, 0.2, 0.15, 0.1, 0.05, 0.04, 0.03, 0.02, 0.01};
 	double pt_bins[10] = {250, 275, 300, 350, 400, 450, 550, 700, 900, 10000};
@@ -60,9 +73,14 @@ void SIMP_QCD_closure(){
 	double err_eff[11] = {0,0,0,0,0,0,0,0,0,0,0};
 	double err_effboth[11] = {0,0,0,0,0,0,0,0,0,0,0};
 	double zero[11] = {0,0,0,0,0,0,0,0,0,0,0};
+	
+	double ratio_1leg[11] = {0,0,0,0,0,0,0,0,0,0,0};
+	double ratio_2leg[11] = {0,0,0,0,0,0,0,0,0,0,0};
+	double err_ratio_1leg[11] = {0,0,0,0,0,0,0,0,0,0,0};
+	double err_ratio_2leg[11] = {0,0,0,0,0,0,0,0,0,0,0};
   
 	std::cout<<"Getting the efficiency histos...";
-	TFile* efficiencies = new TFile("eff2D_QCD_random_PUMoriond17.root", "READ");
+	TFile* efficiencies = new TFile("eff2D_QCD_nPixHitsCut_photonVeto_PUMoriond17_vetoBadEvents.root", "READ");
 	TH2D* eff_histos[11];
 	for(int j = 0; j < 11; j++){
 		std::ostringstream strs;
@@ -74,21 +92,34 @@ void SIMP_QCD_closure(){
 	}
 	std::cout<<"done"<<std::endl;
 	
-  TFile *output = new TFile("closure_test_PUMoriond17.root", "RECREATE");
+  TFile *output = new TFile("closure_test_logratio_photonVeto_PUMoriond17_vetoBadEvents.root", "RECREATE");
 	
 // 	double QCD_xsec[6] = {343500, 32050, 6791, 1214, 118.7, 24.91};//Spring16
 	double QCD_xsec[6] = {346400, 32010, 6842, 1203, 120.1, 25.40}; //PUMoriond17
 // 	double QCD_events[5] = {16830696, 19199088, 15621634, 4980387, 3846616};
-	double lumi = 20;
+	double lumi = 23015;
   
 	for (int l = 0; l < 6; l++){
 		TChain* chain = chains[l];
+		chain->SetBranchAddress("nLumi", &LS);
+		chain->SetBranchAddress("nEvent", &event);
 		chain->SetBranchAddress("jet_pt", &jet_pt);
 		chain->SetBranchAddress("jet_eta", &jet_eta);
 		chain->SetBranchAddress("jet_phi", &jet_phi);
 		chain->SetBranchAddress("jet_efrac_ch_Had", &jet_efrac_ch_Had);
 		chain->SetBranchAddress("jet_efrac_ch_EM", &jet_efrac_ch_EM);
 		chain->SetBranchAddress("jet_efrac_ch_Mu", &jet_efrac_ch_Mu);
+		chain->SetBranchAddress("track_nPixHits", &npixhits);
+    chain->SetBranchAddress("track_dxy", &track_dxy);
+    chain->SetBranchAddress("track_d0", &track_d0);
+    chain->SetBranchAddress("track_dz", &track_dz);
+    chain->SetBranchAddress("photon_pt", &photon_pt);
+    chain->SetBranchAddress("photon_eta", &photon_eta);
+    chain->SetBranchAddress("photon_phi", &photon_phi);
+		chain->SetBranchAddress("photon_passLooseId",&passLooseId);
+		chain->SetBranchAddress("photon_passMediumId",&passMediumId);
+		chain->SetBranchAddress("photon_passTightId",&passTightId);
+  	chain->SetBranchAddress("HLT_DiCentralPFJet170", &dijet_170);
 // 		chain->SetBranchAddress("track_pt", &track_pt);
 // 		chain->SetBranchAddress("track_ptError", &track_ptError);
 		
@@ -98,20 +129,39 @@ void SIMP_QCD_closure(){
 		std::cout<<"QCD bin "<<l<<": Processing "<<Nentries<<" entries with weight "<<weight<<std::endl;
 		
 		for(Int_t entry = 0; entry < Nentries; ++entry){
-			if(entry%1000000==0) std::cout<<"processed "<<entry/1000000<<"events"<<std::endl;
+			if(entry%1000000==0) std::cout<<"processed "<<entry/1000000<<"M events"<<std::endl;
 			chain->GetEntry(entry);
 			
 			double deltajet_phi = jet_phi[0] - jet_phi[1];
 			if(deltajet_phi > TMath::Pi()) deltajet_phi -= 2*TMath::Pi();
 			if(deltajet_phi < -TMath::Pi()) deltajet_phi += 2*TMath::Pi();
+			deltajet_phi = fabs(deltajet_phi);
+			
+			double deltaphi_jet1photon = jet_phi[0] - photon_phi[0];
+			if(deltaphi_jet1photon > TMath::Pi()) deltaphi_jet1photon -= 2*TMath::Pi();
+			if(deltaphi_jet1photon < -TMath::Pi()) deltaphi_jet1photon += 2*TMath::Pi();
+			double deltaphi_jet2photon = jet_phi[1] - photon_phi[0];
+			if(deltaphi_jet2photon > TMath::Pi()) deltaphi_jet2photon -= 2*TMath::Pi();
+			if(deltaphi_jet2photon < -TMath::Pi()) deltaphi_jet2photon += 2*TMath::Pi();
+			
+			double deltaeta_jet1photon = jet_eta[0] - photon_eta[0];
+			double deltaeta_jet2photon = jet_eta[1] - photon_eta[0];
+			
+			double dR1 = TMath::Sqrt(deltaphi_jet1photon*deltaphi_jet1photon + deltaeta_jet1photon*deltaeta_jet1photon);
+			double dR2 = TMath::Sqrt(deltaphi_jet2photon*deltaphi_jet2photon + deltaeta_jet2photon*deltaeta_jet2photon);
 			
 			for (int i = 0; i < 8; i++){
 				CHEF_jet[i] = jet_efrac_ch_Had[i]+jet_efrac_ch_EM[i]+jet_efrac_ch_Mu[i];
 			} 
 			
+			if ((LS = =87073 && event == 257442067) || (LS == 7251 && event == 21438613)    || 
+          (LS == 103681 && event == 30654319) || (LS == 146212 && event == 432294950) || 
+          (LS == 38855 && event == 114879137) || (LS == 109821 && event == 324698790) || 
+          (LS == 27232 && event == 80513184)  || (LS == 158233 && event == 467835335)) badEvent = true;
+      
 			output->cd();
 			
-			if (jet_pt[0] > 250 && jet_pt[1] > 250 && fabs(jet_eta[0]) < 2.0 && fabs(jet_eta[1]) < 2.0 && deltajet_phi > 2){
+			if (jet_pt[0] > 250 && jet_pt[1] > 250 && fabs(jet_eta[0]) < 2.0 && fabs(jet_eta[1]) < 2.0 && deltajet_phi > 2 && npixhits[0] > 0 && (passLooseId[0] == 0 || (passLooseId[0] == 1 && dR1 > 0.1 && dR2 > 0.1)) && !badEvent){
 // 				if (track_ptError[0]/track_pt[0] < 0.5){
 					for(int j = 0; j < 11; j++){
 						if (CHEF_jet[0]<chf_cuts[j] && CHEF_jet[1]<chf_cuts[j]){
@@ -120,6 +170,8 @@ void SIMP_QCD_closure(){
 						}
 						double eff1 = eff_histos[j]->GetBinContent(eff_histos[j]->GetXaxis()->FindBin(fabs(jet_eta[0])), eff_histos[j]->GetYaxis()->FindBin(jet_pt[0]));
 						double eff2 = eff_histos[j]->GetBinContent(eff_histos[j]->GetXaxis()->FindBin(fabs(jet_eta[1])), eff_histos[j]->GetYaxis()->FindBin(jet_pt[1]));
+						double erreff1 = eff_histos[j]->GetBinError(eff_histos[j]->GetXaxis()->FindBin(fabs(jet_eta[0])), eff_histos[j]->GetYaxis()->FindBin(jet_pt[0]));
+						double erreff2 = eff_histos[j]->GetBinError(eff_histos[j]->GetXaxis()->FindBin(fabs(jet_eta[1])), eff_histos[j]->GetYaxis()->FindBin(jet_pt[1]));
 	// 					std::cout<<chf_cuts[j]<<std::endl;
 	// 					std::cout<<jet_eta[0]<<"  "<<jet_pt[0]<<" bin "<<eff_histos[j]->GetXaxis()->FindBin(fabs(jet_eta[0]))<<", "<<eff_histos[j]->GetYaxis()->FindBin(jet_pt[0])<<std::endl;
 	// 					std::cout<<jet_eta[1]<<"  "<<jet_pt[1]<<" bin "<<eff_histos[j]->GetXaxis()->FindBin(fabs(jet_eta[1]))<<", "<<eff_histos[j]->GetYaxis()->FindBin(jet_pt[1])<<std::endl;
@@ -129,21 +181,33 @@ void SIMP_QCD_closure(){
 						if (CHEF_jet[0]<chf_cuts[j]) passed_eff2[j]+=weight*eff2;
 						passed_effboth[j]+=eff1*eff2*weight;
 						
-	// 					err_eff1[j] += ;
-	// 					err_eff2[j] += ;
-	// 					err_effboth[j] += ;
+						err_eff1[j] += weight*weight*erreff1*erreff1;
+						err_eff2[j] += weight*weight*erreff2*erreff2;
+						err_effboth[j] += (eff2*eff2*weight*weight*erreff1*erreff1) + (eff1*eff1*weight*weight*erreff2*erreff2);
 					}
 // 				} 
 			}
 		}
 	}
 	for(int j = 0; j < 11; j++){
+		std::cout<<passed_MCtruth[j]<<std::endl;
 		err_MCtruth[j] = TMath::Sqrt(err_MCtruth[j]);
+		err_effboth[j] = TMath::Sqrt(err_effboth[j]);
+		err_eff1[j] = TMath::Sqrt(err_eff1[j]);
+		err_eff2[j] = TMath::Sqrt(err_eff2[j]);
 		err_eff[j] = (err_eff1[j]+err_eff2[j])/2.0 ;
 		passed_eff[j] = (passed_eff1[j]+passed_eff2[j])/2;
+		ratio_1leg[j] = passed_eff[j]/passed_MCtruth[j];
+		ratio_2leg[j] = passed_effboth[j]/passed_MCtruth[j];
+		err_ratio_1leg[j] = ratio_1leg[j]*TMath::Sqrt((err_eff[j]*err_eff[j]/passed_eff[j]/passed_eff[j])+(err_MCtruth[j]*err_MCtruth[j]/passed_MCtruth[j]/passed_MCtruth[j]));
+		err_ratio_2leg[j] = ratio_2leg[j]*TMath::Sqrt((err_effboth[j]*err_effboth[j]/passed_effboth[j]/passed_effboth[j])+(err_MCtruth[j]*err_MCtruth[j]/passed_MCtruth[j]/passed_MCtruth[j]));
 	}
 	TCanvas *c1 = new TCanvas("Closure test", "Closure test");
-	c1->cd();
+  TPad *pad1 = new TPad("pad1","pad1",0,0.3,1,1);
+  pad1->SetBottomMargin(0);
+  pad1->SetLogy(1);
+  pad1->Draw();
+  pad1->cd();
 	TGraphErrors *MC = new TGraphErrors(11, chf_cuts, passed_MCtruth, zero, err_MCtruth);
 	MC->SetTitle("Closure test");
 	MC->GetXaxis()->SetTitle("CHF cut");
@@ -168,7 +232,35 @@ void SIMP_QCD_closure(){
 	twoleg->SetMarkerStyle(20);
 	twoleg->SetMarkerColor(3);
 	twoleg->Draw("P");
-// 	c1->Print("closure.png");
+	
+	c1->cd();
+	TPad *pad2 = new TPad("pad2","pad2",0,0,1,0.3);
+  pad2->SetTopMargin(0);
+  pad2->SetBottomMargin(0.35);
+  pad2->SetLogy();
+  pad2->Draw();
+  pad2->cd();
+	
+	TGraphErrors *ratio_oneleg = new TGraphErrors(11, chf_cuts, ratio_1leg, zero, err_ratio_1leg);
+	ratio_oneleg->SetMarkerStyle(20);
+	ratio_oneleg->SetMarkerColor(2);
+	ratio_oneleg->Draw("AP");
+  ratio_oneleg->SetTitle("");
+  ratio_oneleg->GetYaxis()->SetRangeUser(0.01, 1.5);
+  ratio_oneleg->GetYaxis()->SetTitle("#frac{1/2-leg}{MCtruth}");
+	
+	TGraphErrors *ratio_twoleg = new TGraphErrors(11, chf_cuts, ratio_2leg, zero, err_ratio_2leg);
+	ratio_twoleg->SetMarkerStyle(20);
+	ratio_twoleg->SetMarkerColor(3);
+	ratio_twoleg->Draw("P");
+  
+  TLine *line = new TLine(0,1,0.55,1);
+  line->SetLineColor(kViolet+5);
+  line->SetLineWidth(2);
+	line->Draw();
+	
+  c1->cd();
+	
 	output->Append(c1);
   output->Write();
   output->Close();
