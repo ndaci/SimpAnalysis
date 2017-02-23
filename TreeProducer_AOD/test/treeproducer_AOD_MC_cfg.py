e4d6f1a380ec42f0a2d9f5d3cf5abafb0a765198
Isabelle <isa.dbruyn@gmail.com>
2017-02-23 15:47:16 +0100

    jets without first PV

--- TreeProducer_AOD/test/treeproducer_AOD_MC_cfg.py
+++ TreeProducer_AOD/test/treeproducer_AOD_MC_cfg.py
@@ -51,10 +51,59 @@ my_id_modules = ['RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonI
 for idmod in my_id_modules:
 	setupAllVIDIdsInModule(process,idmod,setupVIDPhotonSelection)
     
+# Remove first PV
+# Create goodOfflinePrimaryVertrices
+# Run CHS
+# Correct jets
+# Tree producer
+######################
+from CommonTools.ParticleFlow.pfPileUp_cfi  import pfPileUp as _pfPileUp
+from CommonTools.ParticleFlow.TopProjectors.pfNoPileUp_cfi import pfNoPileUp as _pfNoPileUp
+from CommonTools.ParticleFlow.goodOfflinePrimaryVertices_cfi import *
+
+
+process.pfPileUpSPV = _pfPileUp.clone(PFCandidates='particleFlowPtrs',
+                                      Vertices = 'SPVgoodOfflinePrimaryVertices',
+                                      checkClosestZVertex = False )
+process.pfNoPileUpSPV = _pfNoPileUp.clone(topCollection = 'pfPileUpSPV',
+                                          bottomCollection = 'particleFlowPtrs' )
+
+process.SPVgoodOfflinePrimaryVertices = goodOfflinePrimaryVertices.clone()
+process.SPVgoodOfflinePrimaryVertices.src = src = cms.InputTag("pfchsSecondPV:SecondOfflinePrimaryVerices")
+
+process.load('SimpAnalysis.PFCHS_SecondPV.pfchs_SecondPV_cfi')
+
+process.load('RecoJets.Configuration.RecoPFJets_cff')
+from RecoJets.JetProducers.ak4PFJets_cfi import ak4PFJets
+
+process.ak4PFCHSJetsSPV = ak4PFJets.clone(
+    rParam = 0.4,
+    jetPtMin = 5.0,
+    src = "pfNoPileUpSPV",
+)
+
+process.ak4SPVPFCHSJetsCorr = process.ak4PFCHSJetsL1.clone()
+process.ak4SPVPFCHSJetsCorr.correctors = cms.VInputTag('ak4PFCHSL1FastL2L3ResidualCorrector')
+process.ak4SPVPFCHSJetsCorr.src = cms.InputTag('ak4PFCHSJetsSPV')
+    
 # Tree producer
 process.load("SimpAnalysis.TreeProducer_AOD.Treeproducer_AOD_cfi") 
 #process.tree.triggerResults = cms.InputTag("TriggerResults", "", "HLT2") #for XXTo4J
-process.p = cms.Path(process.egmPhotonIDSequence * process.tree)
+
+process.treeSPV = process.tree.clone()
+process.treeSPV.pfjetCollection = cms.InputTag("ak4SPVPFCHSJetsCorr")
+process.treeSPV.vertexCollection = cms.InputTag("SPVgoodOfflinePrimaryVertices")
+#############################
+
+process.p = cms.Path(process.pfchsSecondPV
+                    + process.SPVgoodOfflinePrimaryVertices
+                    + process.pfPileUpSPV
+                    + process.pfNoPileUpSPV
+                    + process.ak4PFCHSJetsSPV
+                    + process.ak4SPVPFCHSJetsCorr
+                    + process.treeSPV
+                    + process.egmPhotonIDSequence 
+                    * process.tree)
 
 # Output
 process.TFileService = cms.Service('TFileService',
